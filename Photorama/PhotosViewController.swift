a33a80beddac85617a7f6b02503e48fc795c9913
Eric Arlan Dockery <eric.dockery283@gmail.com>
2017-01-15 23:09:51 -0500

    Started Chapter 19.

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Eric Dockery on 1/15/17.
+//  Copyright © 2017 Eric Dockery. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+}
